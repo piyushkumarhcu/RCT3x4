07fc6f3cf0d88e15b9743d47cfa278fd7d6fa278
Piyush Kumar <50236918+piyushkumarhcu@users.noreply.github.com>
2021-02-18 13:22:11 +0530

    Update run_hls.tcl

--- RCT3x4/vivado_hls/run_hls.tcl
+++ RCT3x4/vivado_hls/run_hls.tcl
@@ -3,12 +3,13 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project proj
+open_project -reset proj
 set_top algo_top
 add_files ./src/algo_top.cpp
 add_files  ./src/algo_top.h
 add_files -tb ./src/algo_top_tb.cpp
-open_solution "solution1"
+
+open_solution -reset "solution1"
 set_part {xcvu9p-flgc2104-2-e} -tool vivado
 create_clock -period 240MHz -name default
 set_clock_uncertainty 30%
@@ -17,3 +18,5 @@ set_clock_uncertainty 30%
 csynth_design
 ##cosim_design
 ##export_design -format ip_catalog
+
+exit
